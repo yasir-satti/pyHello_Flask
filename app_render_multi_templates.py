7ac6d6a265cd5ce4ca050e5804aef26bde9aab59
Yasir Satti <yassir_satti@hotmail.com>
2021-08-17 01:01:09 +0100

    Apps tutorials

---
+++ app_render_multi_templates.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
